d06eefe761ca0b65a982b6fea99d94fee50fdded
rheera <rheera2@uwo.ca>
2023-09-11 16:26:42 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Notes App`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
